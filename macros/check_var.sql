6650cb7327d2ff2e3363b0cfddd233bcc6c4dbc6
Bram Reinders <bramreinders97@gmail.com>
2024-07-11 14:15:52 +0200

    update error message non declared vars

--- macros/check_var.sql
+++ macros/check_var.sql
@@ -1,7 +1,7 @@
 {% macro check_var(var_name) %}
     {%- set var_value = var(var_name) -%}
     {%- if var_value is none or var_value == '' -%}
-        {%- do exceptions.raise_compiler_error("Variable '" ~ var_name ~ "' is not set or is empty. Please specify this variable in dbt_project.yml") -%}
+        {%- do exceptions.raise_compiler_error("Variable '" ~ var_name ~ "' is not set or is empty. Please specify this variable in view_selection_tool/dbt_project.yml") -%}
     {%- else -%}
         {{ log("Variable '" ~ var_name ~ "' is set to: " ~ var_value, info=True) }}
     {%- endif -%}
